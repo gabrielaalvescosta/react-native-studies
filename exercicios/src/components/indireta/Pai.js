d6890a9972b94c149c8a8483edb56c25de282d20
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-05-06 13:16:18 -0300

    Comunicacao indireta e passando funcoes como props

--- exercicios/src/components/indireta/Pai.js
+++ exercicios/src/components/indireta/Pai.js
@@ -6,14 +6,15 @@ export default props => {
 
     // criamos esse state para atribuir o numero gerao na funcao do filho
     const [ num, setNum ] = useState(0);
+    const [ texto, setTexto ] = useState('');
 
-    function exibirNumero(numero) {
+    function exibirNumero(numero, texto) {
         setNum(numero);
     }
 
        return (
            <>
-           <Text>{num}</Text>
+           <Text>{num}{texto}</Text>
            <Filho 
                 funcao={exibirNumero}
                 min={1}
