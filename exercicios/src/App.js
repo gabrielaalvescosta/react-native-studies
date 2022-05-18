d85ceaca184f9b4a2c46bf6a63563f35dfe156e3
Gabriela Costa <gabrielasilvaalvescosta@gmail.com>
2022-05-18 13:46:54 -0300

    Criando componente em classe

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,68 +1,18 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import Mega from './components/megasena/Mega';
-// import FlexboxV4 from './components/flexbox/FlexboxV4';
-// import UsuarioLogado from './components/condicional/UsuarioLogado';
-// import ContadorV2 from './components/contador/ContadorV2';
-// import Diferenciar from './components/Diferenciar';
-// import Familia from './components/familia/Familia';
-// import Membros from './components/familia/Membros';
-// import Parimpar from './components/Parimpar';
-// import ListaProdutos from './components/produtos/ListaProdutos';
-// import Primeiro from './components/Primeiro';
-// import ComponenteSemNomePadrao, { Componente2, Componente3 } from './components/Multi';
-// import MinMax from './components/MinMax';
-// import Aleatorio from './components/Aleatorio';
-// import Titulo from './components/Titulo';
-// import Botao from './components/Botao';
-// import Contador from './components/Contador';
-// import DigiteSeuNome from './components/controlled/DigiteSeuNome';
+import React, {Component} from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
-function App() {
+
+export default class App extends React.Component {
 
     // console.warn("Console.warn para debug");
-
+    render() {
     return (
-            <SafeAreaView style={style.App}>
-            <Mega qtdeNumeros={6}/>
-            {/* 
-            <FlexboxV4/>
-            <DigiteSeuNome/>
-            <UsuarioLogado usuario={{nome: 'João', email: 'joao@joao.com.br'}} />
-            <ListaProdutosV2/>
-            <ListaProdutos/>
-                        <Parimpar num={3} />
-            <Diferenciar/>
-            <Familia>
-                <Membros nome="Susana" sobrenome="Paiva" />
-                <Membros nome="Joana" sobrenome="Paiva" />
-            </Familia>
-            <Familia>
-                <Membros nome="Marília" sobrenome="José"/>
-            </Familia>
-            <ContadorV2 />
-           <Text>
-               Contador
-           </Text>
-            <Text>
-            <View>
+            <SafeAreaView style={styles.App}>
+            <Text style={styles.t20}> App.js</Text>
+            </SafeAreaView>)
+    }
 
-            <Titulo principal="Cadastro" secundario="Tela de Cadastro"/>
-            <Botao/>
-
-            <MinMax min="3" max="8"/>
-            <Aleatorio min={1} max={60} />
-
-            <Primeiro/>
-            <ComponenteSemNomePadrao/>
-            <Componente2/>
-            <Componente3/>
-            </View>
-          </Text> */}
-          </SafeAreaView>)
 }
-
-export default App;
 
 /*
 
@@ -82,12 +32,15 @@ export default () => <Text>Componente usando arrow function</Text>
 
 */
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
     App: {
         flexGrow: 1,
         justifyContent: "center",
         alignItems: "center",
         padding: 20
         
+    },
+    t20: {
+        fontSize: 20
     }
 })
